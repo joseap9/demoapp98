936169b5641fb1688ea9229c87098bc599b34dbd
Jose Perdomo <joseperdomo2428@gmail.com>
2024-06-14 10:34:25 -0400

    23

--- logic.py
+++ logic.py
@@ -1,30 +1,31 @@
+import xmltodict
 import pandas as pd
-import xml.etree.ElementTree as ET
 
-# Función para parsear el XML y extraer los valores de la etiqueta "Full"
-def parse_xml_to_dataframe(xml_file):
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
+def xml_to_dict(xml_file):
+    with open(xml_file, 'r', encoding='utf-8') as file:
+        xml_content = file.read()
+        return xmltodict.parse(xml_content)
+
+def dict_to_dataframe(xml_dict):
+    # Obtener los registros
+    result_records = xml_dict.get('resultrecords', {}).get('resultrecord', [])
     
-    # Espacios de nombres del XML (debes ajustarlos si es necesario)
-    ns = {
-        'ns': 'https://support.bridgerinsight.lexisnexis.com/downloads/xsd/5.0/ResultsExport.xsd',
-        'i': 'http://www.w3.org/2001/XMLSchema-instance'
-    }
-
-    # Lista para almacenar los valores extraídos
-    full_values = []
-
-    # Iterar sobre todos los elementos que contienen la etiqueta "Full"
-    for elem in root.findall('.//ns:resultrecord', ns):
-        full_tag = elem.find('.//ns:Full', ns)
-        if full_tag is not None:
-            full_values.append(full_tag.text)
-        else:
-            full_values.append(None)
-
-    # Crear un DataFrame con los valores extraídos
-    df = pd.DataFrame(full_values, columns=['Full'])
-    print(df.head(10))
+    # Asegurarse de que result_records sea una lista
+    if not isinstance(result_records, list):
+        result_records = [result_records]
+    
+    # Crear una lista de diccionarios
+    records_list = []
+    for record in result_records:
+        record_dict = {}
+        for key, value in record.items():
+            if isinstance(value, dict) and '#text' in value:
+                record_dict[key] = value['#text']
+            else:
+                record_dict[key] = value
+        records_list.append(record_dict)
+    
+    # Convertir la lista de diccionarios en un DataFrame
+    df = pd.DataFrame(records_list)
     
     return df
